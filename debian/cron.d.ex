bc227b8c8ae977b394ac6f760b37b1999a040c53
John Goerzen <jgoerzen@complete.org>
2007-07-16 15:36:12 -0500

    Removed example files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the gtkrsync package
-#
-0 4	* * *	root	gtkrsync_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(gtkrsync):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="gtkrsync" command="/usr/bin/gtkrsync"
