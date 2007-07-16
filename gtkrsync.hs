45c28ef6376c3e3b61d6225d426b697544cc4ca1
John Goerzen <jgoerzen@complete.org>
2007-07-16 09:57:23 -0500

    More work

--- gtkrsync.hs
+++ gtkrsync.hs
@@ -17,16 +17,29 @@ main = do
     (readfd, writefd) <- createPipe
     pid <- forkProcess (childFunc args rsyncbin readfd writefd)
     closeFd writefd
+    hasExited <- newMVar False
 
     readh <- fdToHandle readfd
     hSetBuffering readh (BlockBuffering Nothing)
 
     rsyncinput <- hGetContents readh
     let rsyncstream = customlines rsyncinput
-    gui <- initRsyncGUI exitApp
-    installHandler sigCHLD (Catch (chldHandler gui pid)) Nothing
+    gui <- initRsyncGUI (exitButton pid hasExited)
+    installHandler sigCHLD (Catch (chldHandler gui pid hasExited)) Nothing
 
     runGUI gui rsyncstream
+
+exitButton pid mv = withMVar mv $ \hasexited ->
+    if hasexited
+       then exitApp
+       else do -- Cancel signal handler since we don't want notification to
+               -- user of exit due to user's own action
+               installHandler sigCHLD Default Nothing
+               -- No need to update the MVar here since there won't be
+               -- anything else to read it.  Besides, doing so would cause
+               -- deadlock anyway.
+               signalProcess sigKILL pid
+               exitApp
 
 childFunc args rsyncbin readfd writefd =
     do closeFd readfd
@@ -35,10 +48,11 @@ childFunc args rsyncbin readfd writefd =
        closeFd writefd
        executeFile rsyncbin True args Nothing
 
-chldHandler gui pid = 
+chldHandler gui pid mv = 
     do ps <- getProcessStatus True False pid
        case ps of
-            Just ps -> case ps of
+            Just ps -> do swapMVar mv True
+                          case ps of
                             Exited 0 -> return ()
                             x -> oobError gui ("rsync exited with unexpected error: " ++ show x)
             Nothing -> return ()
