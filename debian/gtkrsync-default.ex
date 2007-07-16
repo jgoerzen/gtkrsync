7280785459b8fefc3e150edd78b8f9f495891546
John Goerzen <jgoerzen@complete.org>
2007-07-16 15:35:28 -0500

    Added dh_make files

---
+++ debian/gtkrsync-default.ex
@@ -0,0 +1,10 @@
+# Defaults for gtkrsync initscript
+# sourced by /etc/init.d/gtkrsync
+# installed at /etc/default/gtkrsync by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(gtkrsync):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="gtkrsync" command="/usr/bin/gtkrsync"
